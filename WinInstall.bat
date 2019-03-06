3434796ef6dc7114616e4e6e46f13bb9bbbfc7be
Vincenzo Sammartano <v.sammartano@gmail.com>
2019-03-06 09:30:02 +0000

    Update batch file
    Writing a code for the installeation of the prerequisites.

--- WinInstall.bat
+++ WinInstall.bat
@@ -2,7 +2,11 @@
 
 title Install the application on Windows OS
 cls
-echo .... installing prerequisite on your pc ....  
+echo .... installing prerequisite on your pc ....
+echo .... installing Python 
+\\bmuchn000a.bmu.buchermunicipal.biz\homes$\bmusammartanoV\Documents\GitHub\Experimental\PreRequisite\python-3.7.2.exe /SP-
+echo .... installing the library PIP
+pip install Pillow
 echo .... please wait ....
 pause
 
